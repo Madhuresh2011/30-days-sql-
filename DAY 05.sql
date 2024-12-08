066431f5ebd50699a643fec78384388441e949f5
Madhuresh Raj <143110230+Madhuresh2011@users.noreply.github.com>
2024-12-08 13:06:31 +0530

    DAY 05.sql

--- DAY 05.sql
+++ DAY 05.sql
@@ -98,14 +98,5 @@ WHERE drn = 3
 
 
 
-
-
-
-
-/*	
-Follow me in LinkedIn :: https://www.linkedin.com/in/najirr/
-Follow me in insta :: https://www.instagram.com/zero_analyst/
-Subscribe to our youtube channel :: https://www.youtube.com/@zero_analyst
-*/
 	
 
